ffcb71fc03102cfca4535599e55f241ca82c10c8
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2023-11-08 11:51:31 +0600

    create filtering

--- src/Pages/All/All.jsx
+++ src/Pages/All/All.jsx
@@ -1,27 +1,67 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import React, { useState, useEffect } from 'react';
 import SingleBlog from '../../Components/SingleBlog/SingleBlog';
 
 const All = () => {
-    const  {isPending, data} = useQuery({
-        queryKey: ['blogs'],
-        queryFn: async ()=>{
-            const res = await fetch('http://localhost:5000/blogs');
-            return res.json();
-        }
-    })
-    if(isPending){
-        return <span className='loading loading-spinner text-primary'>Loading</span>
+  const { isPending, data } = useQuery({
+    queryKey: ['blogs'],
+    queryFn: async () => {
+      const res = await fetch('http://localhost:5000/blogs');
+      return res.json();
+    },
+  });
+
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const [categoryData, setCategoryData] = useState([]);
+
+  useEffect(() => {
+    console.log('Selected Category:', selectedCategory);
+    if (!isPending) {
+      console.log('Data:', data);
+      if (selectedCategory === 'all') {
+        setCategoryData(data);
+      } else {
+        const foodData = data.filter(item => item.catagory === selectedCategory);
+        setCategoryData(foodData);
+      }
     }
+  }, [selectedCategory, data, isPending]);
+
+  if (isPending) {
+    return <span className='loading loading-spinner text-primary'>Loading</span>;
+  }
+
+  console.log('Category Data:', categoryData);
+
   return (
     <div>
+      <div className='flex justify-between py-3'>
+        <select
+          className='ml-10 input border-slate-300'
+          name="categoryDrop"
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="food">Food</option>
+          <option value="travel">Travel</option>
+          <option value="healthy">Healthy</option>
+        </select>
+
+        <div className="form-control mr-10">
+  <label className="input-group">
+    <input type="text" placeholder="search by title...." className="input input-bordered" />
+    <button className='bg-[#333] text-white px-3'>Search</button>
+  </label>
+</div>
+      </div>
       <div className='grid grid-cols-4 gap-6 mx-10 my-10'>
-      {
-        data.map(blog=> <SingleBlog key={blog._id} blog={blog}/>)
-      }
+        {categoryData.map(blog => (
+          <SingleBlog key={blog._id} blog={blog} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default All
+export default All;
