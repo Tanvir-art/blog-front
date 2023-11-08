cb4dac0eab7425f9ff7bd89932af1b9c4a569e28
Tanvir Hasan <57188940+Tanvir-art@users.noreply.github.com>
2023-11-08 14:40:52 +0600

    add recent data

---
+++ src/Components/Recent/Recent.jsx
@@ -0,0 +1,26 @@
+
+import React, { useEffect, useState } from 'react';
+import RecentDatas from './RecentDatas';
+
+const Recent = () => {
+    const [recentData, setRecentData] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/recent-blogs')
+        .then (res=> res.json())
+        .then(data=> setRecentData(data))
+    })
+      
+  return (
+    <div>
+      <h1 className='text-4xl py-6 font-bold text-center'>Recent Blog</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-10 mb-10'>
+        {
+            recentData.map(iteam=> <RecentDatas key={iteam._id} iteam={iteam}/>)
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Recent
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NewsLetter from '../../Components/NewsLetter/NewsLetter'
 import Hero from '../../Components/hero/hero'
+import Recent from '../../Components/Recent/Recent'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Recent/>
       <NewsLetter/>
     </div>
   )
